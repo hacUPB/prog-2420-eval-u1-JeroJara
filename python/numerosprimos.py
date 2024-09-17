353cd6dceb5b9ba54a3ec3b866ecbe611968970b
JeroJara <B09S202est@MEDAULAS.UPB>
2024-09-17 16:01:45 -0500

    amen

---
+++ python/numerosprimos.py
@@ -0,0 +1,37 @@
+from os import system
+
+control = True
+#programa que dice si un numero es primo o no
+
+while control == True:
+    print("1. Calcular primo \n2. Calcular factorial \n3. Salir")
+    opcion = int(input("ingrese la opción"))
+    if opcion == 1:
+        system("cls")
+        print ("Calcula si el numero es primo o no")
+        numero = int(input("ingrese el número a probar:"))
+        cont = 0
+        for divisor in range(1,numero + 1):
+            if (numero % divisor) == 0:
+                cont += 1
+        if cont > 2:
+         print(f"{numero} no es primo")
+        else:
+         print(f"{numero} sí es primo")
+    elif opcion == 2:
+        system("cls")
+        print("Calcula el factorial del numero")
+        numero = int(input("ingrese el número a probar:"))
+        fact = 1
+        for i in range (1,numero+1):
+            fact *= i
+        print(f"{numero}! = {fact}")
+    elif opcion == 3:
+      control = False
+
+    else:
+       print("Opcion no válida")
+
+
+
+
